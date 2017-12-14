b9b4d1667285bfcf5fab36099bee52c0987d3639
Piotr Lac <piotr.lac@protonmail.ch>
2017-12-15 00:27:50 +0100

    full

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,51 @@
+# postscontroller
 class PostsController < ApplicationController
   def new
-  	@post = Post.new
+    @post = Post.new
   end
 
   def index
-  	@posts = Post.all
+    @posts = Post.all
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def create
-  	@post = Post.new(post_params)
-  	if @post.save
-  		redirect_to post_path(@post)
-  	else
-  		render 'new'
-  	end
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "post zapisany bez przeszkód"
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
-private
-	
-	def post_params
-		params.require(:post).permit(:title, :content)
-	end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "post edytowany bez przeszkód"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "post skasowany bez przeszkód"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-	validates :title, presence: true, length: { minimum: 3, maximum: 10}
-	validates :content, presence: true, length: { minimum: 3, maximum: 10}
+  validates :title, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :content, presence: true, length: { minimum: 3, maximum: 100 }
 end
